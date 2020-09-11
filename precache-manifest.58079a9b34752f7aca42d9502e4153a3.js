d8f66b0d66a3158ec4b535740060f12c4188b929
Hong Quan <quandh619@gmail.com>
2020-09-11 13:29:27 +0700

    Updates

---
+++ precache-manifest.58079a9b34752f7aca42d9502e4153a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c4ad29317d9bba096569466fcea292",
+    "url": "/bluegun/index.html"
+  },
+  {
+    "revision": "bd5ff89f47283e7720d6",
+    "url": "/bluegun/static/css/main.64e66807.chunk.css"
+  },
+  {
+    "revision": "ebfea15298958c41b7ab",
+    "url": "/bluegun/static/js/2.400dc438.chunk.js"
+  },
+  {
+    "revision": "49423731f4b42bb1acb1fefa8741c67d",
+    "url": "/bluegun/static/js/2.400dc438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5ff89f47283e7720d6",
+    "url": "/bluegun/static/js/main.6e671677.chunk.js"
+  },
+  {
+    "revision": "453fb922d336e436cff7",
+    "url": "/bluegun/static/js/runtime-main.5b349f6a.js"
+  }
+]);
